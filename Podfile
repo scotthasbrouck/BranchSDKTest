b79d9953aa1dad0436e40d8df0a254637a493009
Scott Hasbrouck <scott@ginkgotree.com>
2015-01-19 13:28:33 -0500

    Installs Branch SDK

--- Podfile
+++ Podfile
@@ -1,11 +1,12 @@
 # Uncomment this line to define a global platform for your project
-# platform :ios, '6.0'
+platform :ios, '8.0'
 
 target 'BranchSDKTest' do
+    pod 'Branch'
 
 end
 
 target 'BranchSDKTestTests' do
-
+    pod 'Branch'
 end
 
---
+++ Pods/Target Support Files/Pods-BranchSDKTestTests/Pods-BranchSDKTestTests-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// Branch
+#define COCOAPODS_POD_AVAILABLE_Branch
+#define COCOAPODS_VERSION_MAJOR_Branch 0
+#define COCOAPODS_VERSION_MINOR_Branch 3
+#define COCOAPODS_VERSION_PATCH_Branch 96
+
