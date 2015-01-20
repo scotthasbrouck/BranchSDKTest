06c3489d473067744f966cd756fb3e6851524669
Scott Hasbrouck <scott@ginkgotree.com>
2015-01-19 22:22:40 -0500

    Finished install OvershareKit

---
+++ Pods/Target Support Files/Pods-BranchSDKTest/Pods-BranchSDKTest-environment.h
@@ -0,0 +1,32 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// ADNLogin
+#define COCOAPODS_POD_AVAILABLE_ADNLogin
+#define COCOAPODS_VERSION_MAJOR_ADNLogin 2
+#define COCOAPODS_VERSION_MINOR_ADNLogin 1
+#define COCOAPODS_VERSION_PATCH_ADNLogin 0
+
+// Branch
+#define COCOAPODS_POD_AVAILABLE_Branch
+#define COCOAPODS_VERSION_MAJOR_Branch 0
+#define COCOAPODS_VERSION_MINOR_Branch 3
+#define COCOAPODS_VERSION_PATCH_Branch 96
+
+// OvershareKit
+#define COCOAPODS_POD_AVAILABLE_OvershareKit
+#define COCOAPODS_VERSION_MAJOR_OvershareKit 1
+#define COCOAPODS_VERSION_MINOR_OvershareKit 3
+#define COCOAPODS_VERSION_PATCH_OvershareKit 1
+
+// PocketAPI
+#define COCOAPODS_POD_AVAILABLE_PocketAPI
+#define COCOAPODS_VERSION_MAJOR_PocketAPI 1
+#define COCOAPODS_VERSION_MINOR_PocketAPI 0
+#define COCOAPODS_VERSION_PATCH_PocketAPI 3
+
